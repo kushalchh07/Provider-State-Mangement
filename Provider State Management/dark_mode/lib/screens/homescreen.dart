39e3ce0ebdf31c12793419955351384e3a16e4f4
kushalchh07 <kushalchhetri54@gmail.com>
2024-05-27 18:15:08 +0545

    Added darkmode theme app

---
+++ Provider State Management/dark_mode/lib/screens/homescreen.dart
@@ -0,0 +1,43 @@
+import 'package:dark_mode/provider/themechanger_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    print("build");
+    final themeChanger = Provider.of<ThemeChangerProvider>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ThemeChanger"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          RadioListTile<ThemeMode>(
+              title: Text("Light Mode"),
+              value: ThemeMode.light,
+              groupValue: themeChanger.themeMode,
+              onChanged: themeChanger.setTheme),
+          RadioListTile<ThemeMode>(
+              title: Text("Dark Mode"),
+              value: ThemeMode.dark,
+              groupValue: themeChanger.themeMode,
+              onChanged: themeChanger.setTheme),
+          RadioListTile<ThemeMode>(
+              title: Text("System Mode"),
+              value: ThemeMode.system,
+              groupValue: themeChanger.themeMode,
+              onChanged: themeChanger.setTheme),
+        ],
+      ),
+    );
+  }
+}
